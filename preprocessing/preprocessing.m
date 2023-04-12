d9b45a61dcfbb0470a93392d3ce5354982cdab41
Mark <43383309+markthr@users.noreply.github.com>
2023-04-12 12:36:49 -0400

    Completed initial nustft
    Working implementation of nonuniform short time fourier transform along with demo to test it

--- preprocessing/preprocessing.m
+++ preprocessing/preprocessing.m
@@ -1,14 +1,17 @@
 %% Setup
 path = "J:/enee439d/datasets/wisdm-dataset";
 
-time_window = 5; % how often to process a new window in seconds
+window_time = 5; % how often to process a new window in seconds
 max_window_error = 0.1; % the maximum percent a window can be too short by 
+window_time_shift = 1;
+nufft_length = 100;
 % Setting window_memory = 0 indicates that only the current window should be included for processing.
 
 % get letter for the 18 activities, A-S but without N
 activities = char([1:13, 15:19] + 'A' - 1);
 
 time_scale = int64(1E9); % seconds to nanos
+fs = 20;
 %% load directories
 watch_accel = dir(path + "/mat/watch/accel/*.mat");
 watch_gyro = dir(path + "/mat/watch/gyro/*.mat");
@@ -118,19 +121,30 @@ for file_index = 1:numel(watch_accel)
             ds.(fn{i}).TimeStampNanos = ds.(fn{i}).TimeStampNanos + nano_alignments(i);
         end
         
-        % assign 1 second groups
         for i = 1:numel(fn)
-            ds.(fn{i}).Group = idivide(ds.(fn{i}).TimeStampNanos, 10*time_scale);
-        end
-        % get windowed data for each sensor
-        for i = 1:numel(fn)
-            % get first 51 XYZ values as a matrix, flip direction, then
-            % apply FIR filter to get initial values for delays/taps in
-            % order to avoid border effects
-            [~, taps] = filter(hh, 1, flip(xyz_to_mat(ds.(fn{i}), 1:n)), [], 2);
-            ds.(fn{i}).hh_vec = filter(hh, 1, xyz_to_mat(ds.(fn{i})), taps, 2);
-        end
+            X = xyz_to_mat(ds.(fn{i}));
+            [s2, t] = nustft(X, double(ds.(fn{i}).TimeStampNanos)*1E-9, fs, window_time, window_time_shift, max_window_error);
+            f = (0:49)/100*20;
+            subplot(2,2,i)
+            surf(t,f,20*log10(squeeze(s2(:,1,:)).'),'EdgeColor','none');   
+            axis xy; axis tight; view(0,90); c = colorbar;
+            c.Label.String = 'Energy (dB)';
+            xlabel('Time (s)')
+            ylabel('Frequency (Hz)')
+            title([fn{i} '_X'], 'Interpreter', 'none')
 
+        end
+        sgtitle(['Subject: ', int2str(ds.(fn{1}).SubjectID(1)), ', Activity: ', activities(activity_index)])
+        
+        % get windowed data for each sensor
+        % for i = 1:numel(fn)
+        %     % get first 51 XYZ values as a matrix, flip direction, then
+        %     % apply FIR filter to get initial values for delays/taps in
+        %     % order to avoid border effects
+        %     [~, taps] = filter(hh, 1, flip(xyz_to_mat(ds.(fn{i}), 1:n)), [], 2);
+        %     ds.(fn{i}).hh_vec = filter(hh, 1, xyz_to_mat(ds.(fn{i})), taps, 2);
+        % end
+        
         % apply nufft to each set of windowed data
         for i = 1:numel(fn)
             % TODO: this is incredibly inefficient. This should be updated
@@ -140,6 +154,19 @@ for file_index = 1:numel(watch_accel)
         
     end
     break
+end
+%% make plots using nustft
+for i = 1:numel(fn)
+    X = xyz_to_mat(ds.(fn{i}));
+    [s2, t] = nustft(X, double(ds.(fn{i}).TimeStampNanos)*1E-9, fs, window_time, window_time_shift, max_window_error);
+    f = (0:49)/100*20;
+    subplot(2,2,i)
+    surf(t,f,20*log10(squeeze(s2(:,1,:)).'),'EdgeColor','none');   
+    axis xy; axis tight; view(0,90); c = colorbar;
+    c.Label.String = 'Energy (dB)';
+    xlabel('Time (s)')
+    ylabel('Frequency (Hz)')
+    title
 end
 %% make table
 tabulate(ds.p_acc.Group)
@@ -151,10 +178,12 @@ xlabel('Sample')
 ylabel('Time Difference')
 %% a
 Y = nufft(double(ds.p_acc.hh_vec), double(ds.p_acc.TimeStampNanos), [], 2);
+disp(size(Y))
 for group_index = 1:numel(ds.p_acc.Group(end))
     unfft(ds.p_gyr.hh_vec(i,:))
     
 end
+
 %%
 
 %%
@@ -182,7 +211,7 @@ fsst(x,ts,'yaxis')
 
 title('Quadratic Chirp')
 
-%%
+%% function declarations
 function data_struct = load_data_struct(file_struct)
     data_struct = load([file_struct.folder '\' file_struct.name]);
 end
@@ -220,43 +249,43 @@ function matrix = xyz_to_mat(struct, sequence)
     
 end
 
-function [starts, ends] = partition_time_sequence(t, t_win, t_err)
-    starts = zeros(uint32(floor(t(end))), 1, 'uint32');
-    ends = zeros(uint32(floor(t(end))), 1, 'uint32');
+function [starts, ends] = partition_time_sequence(t, t_win, t_err, nufft_length)
+    start_of_sec = zeros(uint32(floor(t(end))), 1, 'uint32');
+    starts = zeros(length(start_of_sec)- t_win, 1, 'uint32');
+    ends = zeros(length(start_of_sec)- t_win, 1, 'uint32');
     
     % get initial value for start times
-    for i = 1:numel(starts)
-        starts(i) = find(t>=(i-1), 1, 'first');
+    for i = 1:numel(start_of_sec)
+        start_of_sec(i) = find(t>=(i-1), 1, 'first');
     end
-    
     % fix sequences to be a multiple of 100 in length
-    for i = (t_win+1):(numel(starts))
-        i_start = start_sec_index(i-t_win);
-        i_end = start_sec_index(i) - 1;
-        seq_len = i_end - i_start;
-        mod_100 = mod(seq_len, 100);
-        if(mod_100 == 0)
+    for i = (t_win+1):(numel(start_of_sec))
+        i_start = start_of_sec(i-t_win);
+        i_end = start_of_sec(i) - 1;
+        seq_len = i_end - i_start + 1;
+        mod_nufft_len = mod(seq_len, nufft_length);
+        if(mod_nufft_len == 0)
             % the window lined up perfectly
-        elseif(seq_len < 100)
+        elseif(seq_len < nufft_length)
             % the window was too short, extend it
             if(i == t_win+1)
                 % cannot extend backwards, extend forwards. 
                 % 
                 % This complicates analysis, but this corner case can happen 
-                % only once peractivity so it should hopefully not be major.
+                % only once per activity so it should hopefully not be major.
                 % TODO: if the data towards the end gets discard, might as well
                 % shift everything forward such that there is enough data
                 % before the first window that backwards extension works
-                i_end = i_end + (100-mod_100);
+                i_end = i_end + (nufft_length-mod_nufft_len);
             else
                 % can extend backwards
-                i_start = i_start - (100-mod_100);
+                i_start = i_start - (nufft_length-mod_nufft_len);
             end
         % if execution reaches here, sequence must be too long
-        elseif(t(i_end) - t(i_start-mod_100) > t_win - t_err)
+        elseif(t(i_end) - t(i_start+mod_nufft_len) > t_win - t_err)
             % Window can be shortened without making the time covered by the
             % window too short.
-            i_start = i_start - mod_100;
+            i_start = i_start + mod_nufft_len;
         else
             % the window is too long, but must be further lengthened to cover
             % the minimum desired timespan while having the samples be a factor
@@ -264,19 +293,42 @@ function [starts, ends] = partition_time_sequence(t, t_win, t_err)
             if(i == t_win+1)
                 % cannot extend backwards, extend forwards. Same drawbacks as
                 % above hold.
-                i_end = i_end + (100-mod_100);
+                i_end = i_end + (nufft_length-mod_nufft_len);
             else
                 % can extend backwards
-                i_start = i_start - (100-mod_100);
+                i_start = i_start - (nufft_length-mod_nufft_len);
             end
         end
-        starts(i) = i_start;
-        ends(i) = i_end;
+        starts(i-t_win) = i_start;
+        ends(i-t_win) = i_end;
+        if(i_end - i_start + 1 ~= nufft_length)
+            disp('[Start, End]')
+            disp([i_start, i_end])
+            disp(t(i_end) - t(i_start))
+        end
     end
 end
 
 % TODO: current, only handles overlap times of multiples of 1 second
-function [s2, f, t] = nustft(x, t, f, dim, FFTLength, TimeWindow, TimeOverlap)
-    [starts, ends] = partition_time_sequence(t, TimeWindow, TimeWindow * max_window_error)
+function [s2, t] = nustft(x, t, fs, window_time, window_time_shift, max_window_error)
+    nufft_length = 50;
+    [starts, ends] = partition_time_sequence(t, window_time, window_time * max_window_error, nufft_length*2);
     
+    [x_dim, ~] = size(x);
+    window_indices = 1:window_time_shift:numel(starts);
+    s2 = zeros(nufft_length, x_dim, nufft_length);
+    for i = window_indices
+        n = ends(i) - starts(i) + 1;
+        f = double(0:(n/2-1))/double(n)*fs;
+        Y = nufft(x(:,starts(i):ends(i)), t(starts(i):ends(i)), f, 2);
+        bin_size = idivide(ends(i)-starts(i)+1, nufft_length*2);
+        if(bin_size == 1)
+            s2(i,:,:) = abs(Y .* conj(Y));
+        else
+            disp('Binning!')
+            disp(bin_size)
+            s2(i,:,:) = sum(reshape(abs(Y.*conj(Y)),bin_size,[], x_dim))./double(bin_size);
+        end
+    end
+    t = t(ends);
 end
