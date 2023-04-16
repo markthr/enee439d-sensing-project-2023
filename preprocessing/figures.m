026a70c559d42d7d35467fb4bfcd2a5619bf4391
Mark <markbthr@gmail.com>
2023-04-16 01:34:26 -0400

    Fixed issue in time partitioning
    Fixed an issue that would occur in the nustft when a full time window passed without any new inputs

--- preprocessing/figures.m
+++ preprocessing/figures.m
@@ -239,13 +239,13 @@ ds = align_sensor_times(ds, time_scale);
 
 fig = figure;
 fig.Position = [800 400 800 600];
-sensor = ds.w_acc;
 f = (0:49)/100*20;
 for i = 1:numel(fn)
+    sensor = ds.(fn{i});
     X = xyz_to_mat(sensor);
-    t = double(ds.(fn{i}).TimeStampNanos)*1E-9;
+    t = double(sensor.TimeStampNanos)*1E-9;
     [s2, t] = nustft(X, t, fs, window_time, window_time_shift, max_window_error);
-    d
+
     subplot(2,2,i)
     surf(t,f,20*log10(squeeze(s2(:,1,:)).'),'EdgeColor','none');   
     axis xy; axis tight; view(0,90); c = colorbar;
@@ -391,13 +391,15 @@ function matrix = xyz_to_mat(struct, sequence)
 end
 
 function [starts, ends, targets] = partition_time_sequence(t, t_win, t_err, nufft_length)
-    start_of_sec = zeros(uint32(floor(t(end))), 1, 'uint32');
+    t_0 = floor(t(1));
+    t_f = floor(t(end));
+    start_of_sec = zeros(uint32(t_f - t_0), 1, 'uint32');
     starts = zeros(length(start_of_sec)- t_win, 1, 'uint32');
     ends = zeros(length(start_of_sec)- t_win, 1, 'uint32');
-    targets = 5:(numel(start_of_sec)-1);
+    targets = (t_0+4):t_f;
     % get initial value for start times
     for i = 1:numel(start_of_sec)
-        start_of_sec(i) = find(t>=(i-1), 1, 'first');
+        start_of_sec(i) = find(t>=t_0+i-1, 1, 'first');
     end
     % fix sequences to be a multiple of 100 in length
     for i = (t_win+1):(numel(start_of_sec))
@@ -405,11 +407,12 @@ function [starts, ends, targets] = partition_time_sequence(t, t_win, t_err, nuff
         i_end = start_of_sec(i) - 1;
         seq_len = i_end - i_start + 1;
         mod_nufft_len = mod(seq_len, nufft_length);
+        error = nufft_length-mod_nufft_len;
         if(mod_nufft_len == 0)
             % the window lined up perfectly
         elseif(seq_len < nufft_length)
             % the window was too short, extend it
-            if(i_start - (nufft_length-mod_nufft_len) <= 0)
+            if(i_start - error <= 0)
                 % cannot extend backwards, extend forwards. 
                 % 
                 % This complicates analysis, but this corner case can happen 
@@ -417,13 +420,20 @@ function [starts, ends, targets] = partition_time_sequence(t, t_win, t_err, nuff
                 % TODO: if the data towards the end gets discard, might as well
                 % shift everything forward such that there is enough data
                 % before the first window that backwards extension works
-                i_end = i_end + (nufft_length-mod_nufft_len);
+                i_end = i_end + error;
+            elseif(i_start > i_end)
+                % This case occurs when an entire time window passes
+                % without a new measurement being acquired. This is handled
+                % by just leaving the output steady until new input is
+                % received.
+                % TODO: is there a more clever way to handle this?
+                i_end = i_start;
+                i_start = i_end - 100 + 1;
             else
                 % can extend backwards
-                i_start = i_start - (nufft_length-mod_nufft_len);
+                i_start = i_start - error;
             end
-        % if execution reaches here, sequence must be too long
-        elseif(t(i_end) - t(i_start+mod_nufft_len) > t_win - t_err)
+        elseif(t(i_end) - t(i_start+mod_nufft_len) > t_win - t_err) % if this guard evaluated, must be too long
             % Window can be shortened without making the time covered by the
             % window too short.
             i_start = i_start + mod_nufft_len;
@@ -431,13 +441,13 @@ function [starts, ends, targets] = partition_time_sequence(t, t_win, t_err, nuff
             % the window is too long, but must be further lengthened to cover
             % the minimum desired timespan while having the samples be a factor
             % of 100
-            if(i_start - (nufft_length-mod_nufft_len) <= 0)
-                % cannot extend backwards, extend forwards. Same drawbacks as
-                % above hold.
-                i_end = i_end + (nufft_length-mod_nufft_len);
+            if(i_start - error <= 0)
+                % cannot extend backwards, shrink forwards. Similar drawbacks as
+                % before hold.
+                i_end = i_end - mod_nufft_len;
             else
                 % can extend backwards
-                i_start = i_start - (nufft_length-mod_nufft_len);
+                i_start = i_start - error;
             end
         end
         starts(i-t_win) = i_start;
@@ -451,10 +461,12 @@ function [s2, t] = nustft(x, t, fs, window_time, window_time_shift, max_window_e
     [starts, ends, targets] = partition_time_sequence(t, window_time, window_time * max_window_error, nufft_length);
     [x_dim, ~] = size(x);
     window_indices = 1:window_time_shift:numel(starts);
-    s2 = zeros(nufft_length/2, x_dim, nufft_length/2);
+    s2 = zeros(length(targets), x_dim, nufft_length/2);
     for i = window_indices
         n = ends(i) - starts(i) + 1;
         f = double(0:(n/2-1))/double(n)*fs;
+        x(:,starts(i):ends(i));
+        t(starts(i):ends(i));
         Y = nufft(x(:,starts(i):ends(i)), t(starts(i):ends(i)), f, 2);
         bin_size = idivide(ends(i)-starts(i)+1, nufft_length);
         if(bin_size == 1)
--- preprocessing/preprocessing.m
+++ preprocessing/preprocessing.m
@@ -218,13 +218,15 @@ function matrix = xyz_to_mat(struct, sequence)
 end
 
 function [starts, ends, targets] = partition_time_sequence(t, t_win, t_err, nufft_length)
-    start_of_sec = zeros(uint32(floor(t(end))), 1, 'uint32');
+    t_0 = floor(t(1));
+    t_f = floor(t(end));
+    start_of_sec = zeros(uint32(t_f - t_0), 1, 'uint32');
     starts = zeros(length(start_of_sec)- t_win, 1, 'uint32');
     ends = zeros(length(start_of_sec)- t_win, 1, 'uint32');
-    targets = 5:(numel(start_of_sec)-1);
+    targets = (t_0+4):t_f;
     % get initial value for start times
     for i = 1:numel(start_of_sec)
-        start_of_sec(i) = find(t>=(i-1), 1, 'first');
+        start_of_sec(i) = find(t>=t_0+i-1, 1, 'first');
     end
     % fix sequences to be a multiple of 100 in length
     for i = (t_win+1):(numel(start_of_sec))
@@ -232,11 +234,12 @@ function [starts, ends, targets] = partition_time_sequence(t, t_win, t_err, nuff
         i_end = start_of_sec(i) - 1;
         seq_len = i_end - i_start + 1;
         mod_nufft_len = mod(seq_len, nufft_length);
+        error = nufft_length-mod_nufft_len;
         if(mod_nufft_len == 0)
             % the window lined up perfectly
         elseif(seq_len < nufft_length)
             % the window was too short, extend it
-            if(i_start - (nufft_length-mod_nufft_len) <= 0)
+            if(i_start - error <= 0)
                 % cannot extend backwards, extend forwards. 
                 % 
                 % This complicates analysis, but this corner case can happen 
@@ -244,13 +247,20 @@ function [starts, ends, targets] = partition_time_sequence(t, t_win, t_err, nuff
                 % TODO: if the data towards the end gets discard, might as well
                 % shift everything forward such that there is enough data
                 % before the first window that backwards extension works
-                i_end = i_end + (nufft_length-mod_nufft_len);
+                i_end = i_end + error;
+            elseif(i_start > i_end)
+                % This case occurs when an entire time window passes
+                % without a new measurement being acquired. This is handled
+                % by just leaving the output steady until new input is
+                % received.
+                % TODO: is there a more clever way to handle this?
+                i_end = i_start;
+                i_start = i_end - 100 + 1;
             else
                 % can extend backwards
-                i_start = i_start - (nufft_length-mod_nufft_len);
+                i_start = i_start - error;
             end
-        % if execution reaches here, sequence must be too long
-        elseif(t(i_end) - t(i_start+mod_nufft_len) > t_win - t_err)
+        elseif(t(i_end) - t(i_start+mod_nufft_len) > t_win - t_err) % if this guard evaluated, must be too long
             % Window can be shortened without making the time covered by the
             % window too short.
             i_start = i_start + mod_nufft_len;
@@ -258,13 +268,13 @@ function [starts, ends, targets] = partition_time_sequence(t, t_win, t_err, nuff
             % the window is too long, but must be further lengthened to cover
             % the minimum desired timespan while having the samples be a factor
             % of 100
-            if(i_start - (nufft_length-mod_nufft_len) <= 0)
-                % cannot extend backwards, extend forwards. Same drawbacks as
-                % above hold.
-                i_end = i_end + (nufft_length-mod_nufft_len);
+            if(i_start - error <= 0)
+                % cannot extend backwards, shrink forwards. Similar drawbacks as
+                % before hold.
+                i_end = i_end - mod_nufft_len;
             else
                 % can extend backwards
-                i_start = i_start - (nufft_length-mod_nufft_len);
+                i_start = i_start - error;
             end
         end
         starts(i-t_win) = i_start;
